87b583706727d0de08670d23f7522b2c20748610
tomieP <nhphuc282@gmail.com>
2025-08-29 09:19:24 +0700

    src

--- src/config.py
+++ src/config.py
@@ -7,13 +7,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
     DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1']
     TESTING = os.environ.get('TESTING', 'False').lower() in ['true', '1']
-    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB'
+    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa123456@127.0.0.1:1433/FlaskApiDB'
     CORS_HEADERS = 'Content-Type'
 
 class DevelopmentConfig(Config):
     """Development configuration."""
     DEBUG = True
-    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB'
+    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa123456@127.0.0.1:1433/FlaskApiDB'
 
 
 class TestingConfig(Config):
