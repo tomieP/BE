70d18ce4e2a24b9da26e2ed6ae2a7988f46926b1
tomieP <nhphuc282@gmail.com>
2025-09-05 00:03:25 +0700

    update apprasal

--- src/infrastructure/models/apprasal_model.py
+++ src/infrastructure/models/apprasal_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer,ForeignKey, String, DateTime, Boolean
+from sqlalchemy import Column, Integer,ForeignKey, String, DateTime, Boolean,Float
 from infrastructure.databases.base import Base
 
 class AppraisalModel(Base):
@@ -8,7 +8,7 @@ class AppraisalModel(Base):
     id = Column(Integer, primary_key=True)
     watchid = Column(Integer,ForeignKey('watch.id'))
     appraiserid = Column(Integer,ForeignKey('appraiser.id'))
-    value_estimate = Column(float,nullable= False)
+    value_estimate = Column(Float,nullable= False)
     condition_note = Column(String(100),nullable=False)
     authenticated = Column(Boolean,nullable= False)
     created_at = Column(DateTime)
